2aa7d9cbe5e789b1aef3e0a56a28ba501a1c048e
VitaliySaburdo <vsaburdo@gmail.com>
2023-01-05 19:38:17 +0200

    13

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,9 +1,19 @@
 import PropTypes from 'prop-types';
-export const Profile = ({ username, tag, location, avatar, stats }) => {
+import css from './Profile.module.css';
+
+export const Profile = ({
+  username,
+  tag,
+  location,
+  avatar,
+  followers,
+  views,
+  likes,
+}) => {
   return (
-    <div key={1} className="profile">
-      <div className="description">
-        <img src={avatar} alt="User avatar" className="avatar" />
+    <div key={username} className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt="User avatar" className={css.avatar} />
         <p className="name">{username}</p>
         <p className="tag">{tag}</p>
         <p className="location">{location}</p>
@@ -11,15 +21,15 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
       <ul className="stats">
         <li>
           <span className="label">Followers</span>
-          <span className="quantity">{stats.followers}</span>
+          <span className="quantity">{followers}</span>
         </li>
         <li>
           <span className="label">Views</span>
-          <span className="quantity">{stats.views}</span>
+          <span className="quantity">{views}</span>
         </li>
         <li>
           <span className="label">Likes</span>
-          <span className="quantity">{stats.likes}</span>
+          <span className="quantity">{likes}</span>
         </li>
       </ul>
     </div>
@@ -31,7 +41,6 @@ Profile.propTypes = {
   tag: PropTypes.string,
   location: PropTypes.string,
   avatar: PropTypes.string,
-  stats: PropTypes.object,
   followers: PropTypes.number,
   views: PropTypes.number,
   likes: PropTypes.number,
