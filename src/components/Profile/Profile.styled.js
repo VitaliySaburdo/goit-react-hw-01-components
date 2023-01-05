59273d5cf6ffd3af31d6de1010ffd1050d67922f
VitaliySaburdo <vsaburdo@gmail.com>
2023-01-06 00:04:06 +0200

    qwq

--- src/components/Profile/Profile.styled.js
+++ src/components/Profile/Profile.styled.js
@@ -11,6 +11,8 @@ export const Description = styled.div`
   display: block;
   margin-left: auto;
   margin-right: auto;
+  padding-right: 20px;
+  padding-left: 20px;
 `;
 
 export const ImgAvatar = styled.img`
@@ -20,7 +22,10 @@ export const ImgAvatar = styled.img`
   border-radius: 50%;
   width: 120px;
   height: 120px;
+  margin-bottom: 20px;
 `;
 export const InfoBlock = styled.p`
   margin: 0;
+  text-align: center;
+  margin-bottom: 10px;
 `;
